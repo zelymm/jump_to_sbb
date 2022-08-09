4ae6128a8a031d42f77611542cbb6fb41f01761f
zelymm <dkdlwkrz@naver.com>
2022-08-10 01:14:52 +0900

    [feat]: create delete func for registered articles

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -170,6 +170,23 @@ public class MainController {
 
         return "%d번 게시물을 수정하였습니다.".formatted(article.getId());
     }
+
+    @GetMapping("/deleteArticle/{id}")
+    @ResponseBody
+    public String deleteArticle(@PathVariable int id) {
+        Article article = articles
+                .stream()
+                .filter(a -> a.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if (article == null) {
+            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+        }
+
+        articles.remove(article);
+        return "%d번 게시물을 삭제하였습니다.".formatted(article.getId());
+    }
 }
 
 @AllArgsConstructor
