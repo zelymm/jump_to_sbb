7dbf0ce0fe9f90a489ccc67521310aff3b4804fb
zelymm <dkdlwkrz@naver.com>
2022-08-10 11:53:05 +0900

    [feat]: create Question Entity and Answer table

---
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.ll.exam.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
