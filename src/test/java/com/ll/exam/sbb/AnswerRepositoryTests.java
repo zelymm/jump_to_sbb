0a28249f406369ebd3a9e71d4ab1e336f34df8b7
zelymm <dkdlwkrz@naver.com>
2022-08-12 01:36:58 +0900

    [refactor]: remove code duplication using static method

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -22,22 +22,12 @@ public class AnswerRepositoryTests {
     }
 
     private void createSampleData() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q2);
+        QuestionRepositoryTests.createSampleData(questionRepository);
     }
 
     private void clearData() {
+        QuestionRepositoryTests.clearData(questionRepository);
         questionRepository.disableForeignKeyChecks();
-        questionRepository.truncate();
         answerRepository.truncate(); //for AR
         questionRepository.enableForeignKeyChecks();
     }
--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -22,7 +22,7 @@ public class QuestionRepositoryTests {
         createSampleData();
     }
 
-    private void createSampleData() {
+    public static int createSampleData(QuestionRepository questionRepository) {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -35,13 +35,20 @@ public class QuestionRepositoryTests {
         q2.setCreateDate(LocalDateTime.now());
         questionRepository.save(q2);
 
-        lastSampleDataId = q2.getId();
+        return q2.getId();
+    }
+    private void createSampleData() {
+        lastSampleDataId = createSampleData(questionRepository);
     }
 
-    private void clearData() {
+    public static void clearData(QuestionRepository questionRepository) {
         questionRepository.disableForeignKeyChecks();
         questionRepository.truncate();
         questionRepository.enableForeignKeyChecks();
+    }
+
+    private void clearData() {
+        clearData(questionRepository);
     }
 
     @Test
