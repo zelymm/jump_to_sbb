ff7abb1f84bf6f7fa9661c39057965a0f96a611b
zelymm <dkdlwkrz@naver.com>
2022-08-12 00:26:29 +0900

    [test]: use BeforeEach for an independent environment of test cases

--- src/main/java/com/ll/exam/sbb/Answer.java
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -20,5 +20,6 @@ public class Answer {
     private LocalDateTime createDate;
 
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Question question;
 }
