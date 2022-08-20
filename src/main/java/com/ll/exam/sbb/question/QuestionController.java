6b891bd2d5ed3baf03e267372cb96c03170e34ed
zelymm <dkdlwkrz@naver.com>
2022-08-20 23:58:52 +0900

    [feat]: link to Q_list_page after registration

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -39,5 +40,11 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(String subject, String content) {
+        questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,5 +19,13 @@ public class QuestionService {
         return questionRepository.findById(id)
                 //orElseThrow 적용
                 .orElseThrow(() -> new DataNotFoundException("no %d question not found,".formatted(id)));
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
     }
 }
