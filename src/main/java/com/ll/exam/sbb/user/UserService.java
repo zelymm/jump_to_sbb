b6f35428040c4d81d30d40165de3149ceb270575
zelymm <dkdlwkrz@naver.com>
2022-08-22 03:25:57 +0900

    [feat]: user repos and service/[test]: create method of UserService

---
+++ src/main/java/com/ll/exam/sbb/user/UserService.java
@@ -0,0 +1,23 @@
+package com.ll.exam.sbb.user;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
