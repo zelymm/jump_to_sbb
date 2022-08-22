60dfe543a137ab4dd763afb4f45347255c09a286
zelymm <dkdlwkrz@naver.com>
2022-08-22 10:14:32 +0900

    [feat]: create form, controller, and template for sign up stage

--- src/main/java/com/ll/exam/sbb/user/UserService.java
+++ src/main/java/com/ll/exam/sbb/user/UserService.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sbb.user;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
